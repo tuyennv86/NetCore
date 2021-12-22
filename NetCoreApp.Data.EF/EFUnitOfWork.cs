149046bc7c99a7464b565d6a1b056d9d5482b545
traicaugieht <traicaugieht@gmail.com>
2021-12-22 11:15:54 +0700

    bai 12

---
+++ NetCoreApp.Data.EF/EFUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using NetCoreApp.Infrastructure.Interfaces;
+
+namespace NetCoreApp.Data.EF
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public EFUnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ NetCoreApp.Infrastructure/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreApp.Infrastructure.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Call Save change fo db context
+        /// </summary>
+        void Commit();
+    }
+}
